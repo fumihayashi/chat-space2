b6f4171ae8057a92aba1e5aa84416b200b580ee2
fumihayashi <fumi.hayashi@di-v.co.jp>
2020-05-04 14:15:18 +0900

    implement json

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,42 @@
-$(function(){
+$(function(){ 
+  function buildHTML(message){
+    if ( message.image ) {
+      var html =
+        `<div class="message">
+          <div class="chat-main__messages--info">
+            <div class="chat-main__messages--info-group-name">
+              ${message.user_name}
+            </div>
+            <div class="chat-main__messages--info-date">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="chat-main__messages--date-message">
+            ${message.content}
+          </p>
+          </div>
+          <img src=${message.image} >
+        </div>`
+      return html;
+    } else {
+      var html =
+        `<div class="message">
+          <div class="chat-main__messages--info">
+            <div class="chat-main__messages--info-group-name">
+              ${message.user_name}
+            </div>
+            <div class="chat-main__messages--info-date">
+              ${message.created_at}
+            </div>
+          </div>
+          <p class="chat-main__messages--date-message">
+            ${message.content}
+          </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
